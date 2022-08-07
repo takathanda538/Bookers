65cc3032f0d4d40a98a3ecb70a77f49bd9f75123
takathanda538 <106585979+takathanda538@users.noreply.github.com>
2022-08-07 07:19:38 +0000

    [Update]bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,16 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
@@ -15,28 +20,32 @@ class BooksController < ApplicationController
   end
 
   def show
-    @book = Book.find_by(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
-    @book = Book.find_by(params[:id])
+    @book = Book.find(params[:id])
   end
-  
+
   def update
-    book = Book.find_by(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
-  
+
   def destroy
-    book = Book.find_by(params[:id])
+    book = Book.find(params[:id])
     book.destroy
-    redirect_to "/books"
+    redirect_to "/books", alert: '投稿を削除しました'
   end
-  
-  
+
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
